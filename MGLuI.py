8f5a4e21dcec6d097faa569bb7d0d4820ea5da39
m-amal <amkerind@protonmail.com>
2022-04-12 14:53:43 +0530

    Added new feature-Edit books in database

--- MGLuI.py
+++ MGLuI.py
@@ -194,6 +194,7 @@ def delete_books_window_fun():
     #button to delete books from database
     delete_books_button=Button(delete_books_window, text="Delete", command=delete_books_fun)
 
+
     #grid layout
 
     #label
@@ -207,10 +208,149 @@ def delete_books_window_fun():
     delete_books_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10, ipadx=150)
     
 
+#function to print detais of the searched book from book database
+def edit_books_search_fun():
+    #connecting to database
+    book_conn = sqlite3.connect('books.db')
+    book_csr=book_conn.cursor()
 
+    #fetching details from database
+    book_records_id=edit_uid_search_box.get()
+    book_csr.execute("SELECT * FROM book WHERE oid="+book_records_id)
+    book_records=book_csr.fetchall()
+
+    for book_records in book_records:
+        edit_book_no_box.insert(0, book_records[0])
+        edit_title_box.insert(0, book_records[1])
+        edit_author_box.insert(0, book_records[2])
+        edit_language_box.insert(0, book_records[3])
+        edit_original_text_box.insert(0, book_records[4])
+        edit_isbn_box.insert(0, book_records[5])
+        edit_awards_box.insert(0, book_records[6])
+        edit_value_box.insert(0, book_records[7])
+
+    #commiting changes
+    book_conn.commit()
+    #closing connection
+    book_conn.close()
+
+
+
+#function to edit books in book database
+def edit_books_fun():
+    #connecting to database
+    book_conn = sqlite3.connect('books.db')
+    book_csr=book_conn.cursor()
+
+    #editing details in database
+    book_records_id=edit_uid_search_box.get()
+    book_csr.execute("SELECT * FROM book WHERE oid="+book_records_id)
+    book_records=book_csr.fetchall()
+
+    book_csr.execute("""UPDATE book SET
+            book_no = :bno,
+            title = :ttl,
+            author = :aut,
+            language = :lng,
+            original_text = :org,
+            isbn = :sbn,
+            awards = :awd,
+            value = :vlu
+
+            WHERE oid + :oid""",
+            {
+            'bno': edit_book_no_box.get(),
+            'ttl': edit_title_box.get(),
+            'aut': edit_author_box.get(),
+            'lng': edit_language_box.get(),
+            'org': edit_original_text_box.get(),
+            'sbn': edit_isbn_box.get(),
+            'awd': edit_awards_box.get(),
+            'vlu': edit_value_box.get(),
+            'oid': book_records_id
+            })
+    
+    #commiting changes
+    book_conn.commit()
+    #closing connection
+    book_conn.close()
+
+    #clear the text boxes
+    edit_book_no_box.delete(0, END)
+    edit_title_box.delete(0, END)
+    edit_author_box.delete(0, END)
+    edit_language_box.delete(0, END)
+    edit_original_text_box.delete(0, END)
+    edit_isbn_box.delete(0, END)
+    edit_awards_box.delete(0, END)
+    edit_value_box.delete(0, END)
+
+
+    
 #function to open edit books window
 def edit_books_window_fun():
+    
+    global edit_uid_search_box, edit_book_no_box, edit_title_box, edit_author_box, edit_language_box, edit_original_text_box, edit_isbn_box, edit_awards_box, edit_value_box
+    
     edit_books_window=Toplevel()
+
+    #label
+    edit_books_top_label=Label(edit_books_window, text="Edit books in book database")
+    edit_uid_search_label=Label(edit_books_window, text="ID")
+    edit_book_no_label=Label(edit_books_window, text="Book No")
+    edit_title_label=Label(edit_books_window, text="Title")
+    edit_author_label=Label(edit_books_window, text="Author")
+    edit_language_label=Label(edit_books_window, text="Language")
+    edit_original_text_label=Label(edit_books_window, text="Origanal text")
+    edit_isbn_label=Label(edit_books_window, text="ISBN")
+    edit_awards_label=Label(edit_books_window, text="Awards")
+    edit_value_label=Label(edit_books_window, text="Value")
+    
+    #entry box
+    edit_uid_search_box=Entry(edit_books_window, width=30)
+    edit_book_no_box=Entry(edit_books_window, width=30)
+    edit_title_box=Entry(edit_books_window, width=30)
+    edit_author_box=Entry(edit_books_window, width=30)
+    edit_language_box=Entry(edit_books_window, width=30)
+    edit_original_text_box=Entry(edit_books_window, width=30)
+    edit_isbn_box=Entry(edit_books_window, width=30)
+    edit_awards_box=Entry(edit_books_window, width=30)
+    edit_value_box=Entry(edit_books_window, width=30)
+    
+    #button to add book details to database
+    edit_uid_search_button=Button(edit_books_window, text="Search", command=edit_books_search_fun)
+    edit_books_button=Button(edit_books_window, text="Update", command=edit_books_fun)
+
+
+    #grid layout
+    
+    #label
+    edit_books_top_label.grid(row=0, column=0, columnspan=2, pady=5)
+    edit_uid_search_label.grid(row=1,column=0, pady=1)
+    edit_book_no_label.grid(row=3, column=0, pady=1)
+    edit_title_label.grid(row=4, column=0, pady=1)
+    edit_author_label.grid(row=5, column=0, pady=1)
+    edit_language_label.grid(row=6, column=0, pady=1)
+    edit_original_text_label.grid(row=7, column=0, pady=1)
+    edit_isbn_label.grid(row=8, column=0, pady=1)
+    edit_awards_label.grid(row=9, column=0, pady=1)
+    edit_value_label.grid(row=10, column=0, pady=1)
+    
+    #entry box
+    edit_uid_search_box.grid(row=1, column=1)
+    edit_book_no_box.grid(row=3, column=1)
+    edit_title_box.grid(row=4, column=1)
+    edit_author_box.grid(row=5, column=1)
+    edit_language_box.grid(row=6, column=1)
+    edit_original_text_box.grid(row=7, column=1)
+    edit_isbn_box.grid(row=8, column=1)
+    edit_awards_box.grid(row=9, column=1)
+    edit_value_box.grid(row=10, column=1)
+    
+    #button
+    edit_uid_search_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10, ipadx=150)
+    edit_books_button.grid(row=11, column=0, columnspan=2, padx=10, pady=10, ipadx=150)
+    
 
 
 
